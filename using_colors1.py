6e857004ac31f4eca8ae13d2721a73479dc0983a
leogtzr <leogutierrezramirez@gmail.com>
2020-11-11 19:34:34 -0700

    using colors

---
+++ using_colors1.py
@@ -0,0 +1,42 @@
+import curses
+import time
+
+
+def middle_screen_position(scr, text):
+    h, w = scr.getmaxyx()
+    x = (w//2) - (len(text)//2)
+    y = h//2
+    return y, x
+    # h, w = src.getmaxyx()
+
+
+def main(stdscr):
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_YELLOW)
+
+    text = 'Leonardo Gutierrez Ramirez'
+    x, y = middle_screen_position(stdscr, text)
+
+    stdscr.attron(curses.color_pair(1))
+
+    stdscr.addstr(x, y, text)
+    stdscr.attroff(curses.color_pair(1))
+
+    stdscr.refresh()
+    time.sleep(3)
+
+# stdscr = curses.initscr()
+
+# curses.noecho()
+
+# curses.curs_set(0)
+# stdscr.addstr(2, 0, 'Hello\nWorld')
+# stdscr.refresh()
+
+# time.sleep(5)
+
+# curses.curs_set(1)
+# curses.endwin()
+
+
+curses.wrapper(main)
